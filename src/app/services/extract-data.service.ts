989c7c6ac499d23114b66d778fd26b5eada24d17
Rupesh Chiluka <69051864+rupeshchiluka001@users.noreply.github.com>
2021-08-28 16:20:01 +0530

    Added API KEY

--- src/app/services/extract-data.service.ts
+++ src/app/services/extract-data.service.ts
@@ -11,7 +11,7 @@ export class ExtractDataService {
   constructor(private http: HttpClient) {}
 
   getUrl(city: string): string {
-    return `https://api.openweathermap.org/data/2.5/forecast/?q=${city}&units=metric&cnt=40&appid=25fbe48dea65d3e5de62278606235bc7`;
+    return `https://api.openweathermap.org/data/2.5/forecast/?q=${city}&units=metric&cnt=40&appid=${{ secrets.OPENWEATHERMAPAPIKEY }}`;
   }
 
   getData(city: string): Observable<WeatherData> {
